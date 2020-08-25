f9d0e229a1a60e465bd4190e5a838f3b9d12a9ba
worrada <worrada@users.noreply.github.com>
2020-08-25 21:28:52 +0700

    ทำ ContactDatabase.kt

--- app/src/main/java/com/example/myfirstapp/contact/ContactDatabase.kt
+++ app/src/main/java/com/example/myfirstapp/contact/ContactDatabase.kt
@@ -1,4 +1,34 @@
 package com.example.myfirstapp.contact
 
-class ContactDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.myfirstapp.data.ContactData
+
+@Database(entities = [ContactData::class],version = 1)
+abstract class ContactDatabase : RoomDatabase(){
+
+    abstract val conDatabaseDao: ContactDao
+    companion object{
+        private var INSTANCE: ContactDatabase? = null
+
+        fun  getInstance(context: Context): ContactDatabase{
+            synchronized(this){
+                var instance = INSTANCE
+
+                if (instance == null){
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        ContactDatabase::class.java,
+                        "contact_his_database"
+                    )
+                        .fallbackToDestructiveMigration()
+                        .build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
 }
